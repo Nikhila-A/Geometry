2e91de42ba60e305ac2686edc70bcd8fda83344f
Nikhila-A <akuluru.nikhila@shopup.org>
2022-08-03 22:34:36 +0530

    Create Point.rb and Line.rb Files

--- lib/Geometry/version.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-module Geometry
-  VERSION = "0.1.0"
-end
---
+++ spec/line_spec.rb
@@ -0,0 +1,10 @@
+Spec.describe Geometry do
+    it "has a version number" do
+      expect(Geometry::VERSION).not_to be nil
+    end
+  
+    it "does something useful" do
+      expect(false).to eq(true)
+    end
+  end
+  

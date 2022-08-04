5e52d8ca23d95701464eb8a568b57196d2b68650
IswaryaPothula <iswaryapothula@gmail.com>
2022-08-04 11:52:24 +0530

    Upadte line

--- lib/line.rb
+++ lib/line.rb
@@ -2,8 +2,7 @@ require "point.rb"
 module Line
 
     class Cartesianlength
-    #   p1=Point::SinglePoint.new
-    #   p2=Point::SinglePoint.new
+
       def initialize(p1,p2)
         @p1=p1
         @p2=p2
@@ -17,10 +16,11 @@ module Line
         elsif @p1.y == @p2.y
             return (@p2.x - @p1.x).abs
         end
-        
+
         length = Math.sqrt((@p2.x-@p1.x)**2+(@p2.y-@p1.y)**2)
         return length.round(2)
        end
   
     end   
+    
 end
--- spec/point_spec.rb
+++ spec/point_spec.rb
@@ -6,8 +6,10 @@ RSpec.describe Point do
     new_point = Point::SinglePoint.new
     expect(true).to eq(new_point.setattribute(1,0.0))
   end
+
   it "validates whether the points are initialized" do
     new_point = Point::SinglePoint.new
     expect(false).to eq(new_point.setattribute('n','viswa'))
   end
+  
 end
